56dfe31d82e76a3dd2be5206cbef50570b1e86a1
Jake <jakelovitz@gmail.com>
2019-07-08 16:00:14 -0400

    finished move creation form

--- src/selection_page/selection_container.js
+++ src/selection_page/selection_container.js
@@ -1,12 +1,17 @@
 import React from "react";
-import MoveForm from "./move_form";
+import FighterForm from "./fighter_form";
 
 class SelectionContainer extends React.Component {
 
+    handleSubmit = (event) => {
+        event.preventDefault()
+        console.log('hey tim')
+        debugger
+    }
 
     render() {
         return (
-            < MoveForm />
+            < FighterForm handleSubmit={this.handleSubmit}/>
         )
     }
 
