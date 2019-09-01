f90ed3540d3aecfd31a371d37f1e73ede5a00bb6
Jake <jakelovitz@gmail.com>
2019-09-01 15:08:30 -0400

    revamped CSS throughout app

--- src/fight_page/battleground.js
+++ src/fight_page/battleground.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from './container.module.css'
 
 class Battleground extends React.Component {
 
@@ -7,9 +8,12 @@ class Battleground extends React.Component {
         return (
             
             <div>
-            <h1>It's {this.props.activeFighter.name}'s turn</h1>
-            
-            {this.props.image ? <img src={this.props.image} width={400} height={400}></img> : null}
+                <h1>It's {this.props.activeFighter.name}'s turn</h1>
+                
+                {this.props.image ? <img src={this.props.image} className={styles.moveImage}></img> : null}
+
+                {this.props.image ? <h1>Move did {this.props.moveDamage} damage!</h1> : null}
+
             </div>
              
         )
